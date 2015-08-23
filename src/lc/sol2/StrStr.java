6da07ee14ddf93d042f27f4e192314a09f1d5f58
knightseu <sean.xuanlin@gmail.com>
2015-08-23 12:23:59 -0700

    add comments

--- src/lc/sol2/StrStr.java
+++ src/lc/sol2/StrStr.java
@@ -7,4 +7,27 @@ public class StrStr {
 
 	}
 
+	/*
+	 * 1. check input
+	 * 2. coding style, space 
+	 * 3. naming of parameter
+	 * 
+	 */
+	 public String strStr(String src, String dest) {
+		 if (src == null || dest == null) {
+			 return null;
+		 }
+		 int i, j;
+		 for (i = 0; i< src.length() - dest.length() + 1; i++) {
+			 for (j = 0; j < dest.length(); j++) {
+				 if (src.charAt(i+j) != dest.charAt(j)) {
+					 break;
+				 }
+			 }
+			 if (j ==  dest.length()) {
+				 return src.substring(i);
+			 }
+		 }
+		 return null;
+	 }
 }
