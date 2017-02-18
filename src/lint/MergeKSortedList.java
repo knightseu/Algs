597cfea2d86ac4bab2472b97d6ec6d0c831d7d24
knightseu <sean.xuanlin@gmail.com>
2017-02-18 08:25:47 -0800

    jdk

---
+++ src/lint/MergeKSortedList.java
@@ -0,0 +1,34 @@
+package lint;
+
+import common.datastructure.ListNode;
+import java.util.List;
+
+/**
+ * Created by xuanlin on 17/2/18.
+ */
+public class MergeKSortedList {
+    /**
+     * @param lists: a list of ListNode
+     * @return: The head of one sorted list.
+     */
+    public ListNode mergeKLists(List<ListNode> lists) {
+        // write your code here
+    }
+}
+
+
+/*
+Merge k sorted linked lists and return it as one sorted list.
+
+Analyze and describe its complexity.
+
+Example
+Given lists:
+
+[
+  2->4->null,
+  null,
+  -1->null
+],
+return -1->2->4->null.
+ */
